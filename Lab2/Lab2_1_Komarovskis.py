ca05fa81d11a1bf49214e990005f73495ad9da57
Roberts <robertskomarovskis@gmail.com>
2021-02-16 19:20:07 +0200

    Finished first two tasks

---
+++ Lab2/Lab2_1_Komarovskis.py
@@ -0,0 +1,41 @@
+import socket
+import time
+import sys
+import threading 
+
+def listenThread():
+	global s
+	while True:
+		data = s.recv(4096).decode()
+		print(data)
+
+
+port = 8888
+host = "localhost"
+IP_addr = socket.gethostbyname(host)
+
+if len(sys.argv) == 2:
+    IP_addr = sys.argv[1]
+elif len(sys.argv) == 3:
+    IP_addr = sys.argv[1]
+    port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP_addr, port))
+
+
+listen_thread = threading.Thread(target=listenThread, daemon=True)
+listen_thread.start()
+time.sleep(1)
+try:
+	while True:
+		msg = input("Input message: ")	
+		s.sendall(msg.encode())
+		print("Sent: " + msg)
+		time.sleep(0.1)
+
+
+
+except KeyboardInterrupt:
+	s.close()
+
---
+++ Lab2/Lab2_2_Komarovskis.py
@@ -0,0 +1,46 @@
+import socket
+import threading 
+import sys
+
+
+def clientThread(connection):
+	connection.send("\n\nWelcome to my server ".encode())	
+	
+	while True:
+		try:	
+			data = connection.recv(4096)	
+			if not data:
+				break
+			reply = "Your message was " + data.decode()
+			print("Received: " + data.decode())
+			connection.sendall(reply.encode())
+		except socket.error as error_msg:
+			print(error_msg)
+			break
+	connection.close()
+
+port = 8888 
+host = "localhost"
+IP_addr = socket.gethostbyname(host)
+
+if len(sys.argv) > 1:
+	port = int(sys.argv[1])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((IP_addr, port))
+s.listen(10) 			# Number of possible connections
+
+
+try:
+	while True:
+		print("Waiting for conncetion...")
+		connection, address = s.accept()
+		print("New connection...")
+				
+
+		new_thread = threading.Thread(target=clientThread, args = (connection,), daemon=False)
+		new_thread.start()
+except KeyboardInterrupt:
+	s.close()	
+
