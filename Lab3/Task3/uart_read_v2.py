9050d4585eba1d25d8d1db998a94f1483f6a56d0
rokspy <robertskomarovskis@gmail.com>
2021-04-15 16:38:35 +0300

    Finshing Lab3

--- Lab3/Task3/uart_read_v2.py
+++ Lab3/Task3/uart_read_v2.py
@@ -1,5 +1,5 @@
 import serial
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import neopixel
 import threading 
 import sys
@@ -7,13 +7,27 @@ import time
 import board
 
 def threadRoutine(ser):
+    time_count = 0
+    the_time = []
+    temp = []
+
     while True:
         msg = ser.read(5).decode()
         try:
             print(msg)
             led_set(float(msg))
             writeFile(msg)
+
+
         except: continue
+        the_time.append(time_count)
+        temp.append(msg)         
+        plt.plot(the_time, temp, 'b')
+        plt.grid()
+        plt.pause(0.001)
+        plt.grid()
+        time_count += 1    
+
 
 def writeFile(data):
     f = open("temperature.txt", "w")
@@ -23,10 +37,11 @@ def writeFile(data):
 def led_set(rx_data):
     global pixels
     # Range is from 0 to 1023
-    rx_data = (rx_data-18)*60
-    red = rx_data * 255/1023
-    blue = 255 - red
-    pixels[0] = (red, 0, blue)
+    if rx_data > 18 and rx_data < 35:
+        rx_data = (rx_data-18)*60
+        red = rx_data * 255/1023
+        blue = 255 - red
+        pixels[0] = (red, 0, blue)
 
 ser = serial.Serial()
 ser.baudrate = 9600
@@ -39,7 +54,6 @@ pixels = neopixel.NeoPixel(board.D18,1)   # GPIO1, 12 pin on the board
 pixels[0] = (0,0,0) 
 
 
-
 new_thread = threading.Thread(target=threadRoutine, args = (ser,), daemon=True)
 new_thread.start()
 while True:
@@ -50,3 +64,4 @@ while True:
     except KeyboardInterrupt:
         ser.close()
         pixels[0] = (0,0,0)
+        
