107aa93e73a957a1484f27575d7b11ac99e5fcca
rokspy <robertskomarovskis@gmail.com>
2021-05-31 15:26:50 +0300

    Lab6 progress

--- Lab6/Task2/Lab6_2_Komaarovskis_client_world.py
+++ Lab6/Task2/Lab6_2_Komaarovskis_client_world.py
@@ -2,7 +2,8 @@ import serial
 
 ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2)
 while True:
-    msg = ser.read(10).decode()
+
+    msg = ser.read(ser.inwaiting()).decode()
     if len(msg) != 0:
         print(msg)
 
