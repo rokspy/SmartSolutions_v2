a405480f2fea41c8cd6447559025bb26e3a58bc3
Roberts <robertskomarovskis@gmail.com>
2021-06-01 03:09:10 +0300

    Final stages of Lab6

--- Lab6/Task2/Lab6_2_Komarovskis_server/Lab6_2_Komarovskis_server.ino
+++ Lab6/Task2/Lab6_2_Komarovskis_server/Lab6_2_Komarovskis_server.ino
@@ -8,14 +8,17 @@
 
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
-
 WiFiServer server(80);
+WiFiServer server_for_LED(81);
 IPAddress IP(10,10,10,1);
 IPAddress mask = (255, 255, 255, 0);
 
-SoftwareSerial mySer(D1,D2);
-
 byte ledPin = 2;
+
+unsigned long measure_time;
+
+bool timer = 0;
+bool sonar_state = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -23,30 +26,49 @@ void setup() {
   WiFi.softAPConfig(IP, IP, mask);
   WiFi.softAP("rokspy_AP", "Wemos_comm");
   server.begin();
+  server_for_LED.begin();
   pinMode(ledPin, OUTPUT);
   Serial.println();
   Serial.println("accesspoint_bare_01.ino");
   Serial.println("Server started.");
   Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
   Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
-  mySer.begin(9600);
 }
 
 void loop() {
   WiFiClient client = server.available();
-  if (!client) {return;}
-  digitalWrite(ledPin, LOW);
-  if(client.available()){
-    String request = client.readStringUntil('\r');
-    Serial.println("********************************");
-    Serial.println("From the station " + request);
-    mySer.print(request);
-    client.flush();
+  if (client) {
+    digitalWrite(ledPin, LOW);
+    if(client.available()){
+      String request = client.readStringUntil('\r');
+      client.flush();
+      Serial.println("********************************");
+      Serial.println("From the station " + request);
+
+      // If receives signal from PIR, starts timer, enables the led 
+      if(request == '1'){
+        WiFiClient client = server_for_LED.available();
+        if (client) {
+          measure_time = millis();
+          timer = 1;
+          sonar_state = 0;
+          // Enable the LED
+          // ...
+        }
+      }
+      else if(request == '2'){
+        sonar_state ^= 1;
+        if(!timer){
+          // Send LED state to the client. Sonar state determines the LED state
+          // ... 
+        }
+      }
+    }
   }
-//  Serial.print("Byte sent to the station: ");
-//  Serial.println(client.println(request + "ca" + "\r"));
-  String saadetud=Serial.readString();
-  client.println(saadetud+"\r");
-//  Serial.println(saadetud);
+  // If timer is on then waits till it turns off set the appropriate led state
+  if(millis() - measure_time > 30000){ 
+    timer = 0;
+    // Send LED state to the client. Sonar state determines the LED state
+  }
   digitalWrite(ledPin, HIGH);
 }
